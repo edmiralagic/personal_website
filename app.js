f8d27cd3eaf09ea91c9be608cf310178eb1bc2bd
edmiralagic <edmir.alagic@my.ccsu.edu>
2017-05-01 19:40:10 -0400

    changed style

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 function createDot(x, y) {
     let elem = document.createElement("div");
-    if(Math.floor(Math.random()*10) > 7) {
+    if(Math.floor(Math.random()*10) > 6) {
         elem.setAttribute("class", "medium-star");
     } else {
         elem.setAttribute("class", "small-star");
     }
     elem.setAttribute("style", "left:" + x + "px;top:" + y + "px;");
     document.getElementsByTagName("body")[0].appendChild(elem);
+    elem.animate({left: "+=500"}, 2000);
+    elem.animate({left: "-=300"}, 1000);
     return elem;
 }
 
 function anotherDot() {
     let x = Math.floor(Math.random() * window.innerWidth);
-    let y = Math.floor(Math.random() * (window.innerHeight/2));
+    let y = Math.floor(Math.random() * (window.innerHeight/5));
     createDot(x,y);
 }
 
-for(let i = 0; i < 200 ; i++) {
+for(let i = 0; i < 200; i++) {
     anotherDot();
 }
+
